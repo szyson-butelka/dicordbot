4d8554553fa056ccb132e3b4e843177278357ae3
szyson-butelka <91913810+szyson-butelka@users.noreply.github.com>
2021-10-04 22:08:24 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-
-bot.on("ready", function (){
-    console.log(`${bot.user.username} Is Online!`);
-});
-
-bot.login(process.env.token);
